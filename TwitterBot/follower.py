8a6f9d8dce454bc7e6bb8fafecaf0351235a396b
sudhirvaghela <artarea83@gmail.com>
2020-10-18 15:12:38 +0530

    removed alert

--- TwitterBot/follower.py
+++ TwitterBot/follower.py
@@ -29,7 +29,7 @@ class TwitterBott:
         for i in range(1, 10):
             bot.execute_script('window.scrollBy(0,500)')
             time.sleep(10)
-            print('procced is done')
+
 
 
 tb = TwitterBott('username', 'password')
